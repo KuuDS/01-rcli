3c0f7a316ae8e39221a3a4764c80966e695b3494
KuuDS <9947917+KuuDS@users.noreply.github.com>
2024-04-26 07:58:03 +0000

    complete rcli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Ok, Result};
+use clap::Parser;
+use rcli::{process_csv, Opts, SubCommand};
+
+fn main() -> Result<()> {
+    let opts = Opts::parse();
+
+    println!("{:?}", opts);
+
+    match opts.cmd {
+        SubCommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
+    }
+
+    Ok(())
 }
